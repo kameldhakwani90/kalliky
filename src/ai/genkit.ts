0825ff39d189376caf3866981992b5cc014eac05
Mohamed Kamel Dhakwani <medkamel.dhakwani@gmail.com>
2025-07-26 22:24:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,11 @@
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import { config } from 'dotenv';
+
+config();
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-2.0-flash',
+  model: 'googleai/gemini-2.0-flash'
 });
